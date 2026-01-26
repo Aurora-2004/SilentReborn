b195b512bb5438f33dd8467a12e1403295603967
silent ben8x <134549461+Aurora-2004@users.noreply.github.com>
2026-01-26 23:25:18 +0000

    Create Tabs.lua

---
+++ Tabs.lua
@@ -0,0 +1,48 @@
+
+return {
+
+    {
+        Name     = "Player",
+        Icon     = "5012544693",
+        Sections = {},
+    },
+
+    {
+
+        Name     = "Game",
+        Icon     = "6034227061",
+        Sections = {},
+
+    },
+
+    {
+        Name     = "Property",
+        Icon     = "6034687957",
+        Sections = {},
+    },
+
+    {
+
+        Name     = "Auto Buy",
+        Icon     = "6031265983",
+        Sections = {},
+
+    },
+
+    {
+
+        Name     = "Wood",
+        Icon     = "6034503369",
+        Sections = {},
+
+    },
+
+    {
+
+        Name     = "Settings",
+        Icon     = "6031280882",
+        Sections = {},
+
+    },
+
+}
