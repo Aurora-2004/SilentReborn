2adaf8fc3e93f4f4b825b8e7889cbba78311d07c
silent ben8x <134549461+Aurora-2004@users.noreply.github.com>
2026-02-03 12:34:20 +0000

    Create Teleports.lua

---
+++ LumberTycoon2/Teleports.lua
@@ -0,0 +1,26 @@
+
+return {
+    
+    ["Wood R Us"]           = CFrame.new(265, 3, 57),
+    ["Land Store"]          = CFrame.new(258, 3, -99),
+    ["Boxed Cars"]          = CFrame.new(509, 3, -1463),
+    ["Fancy Furnishings"]   = CFrame.new(491, 3, -1720),
+    ["Bob's Shack"]         = CFrame.new(260, 8, -2542),
+    ["Link's Logic"]        = CFrame.new(4607, 7.5, -798),
+    ["Fine Arts Shop"]      = CFrame.new(5207, -166, 719),
+    ["Spawn"]               = CFrame.new(172, 1, 58),
+    ["The Creature's Cave"] = CFrame.new(-481, -11, -171),
+    ["Safari"]              = CFrame.new(115, 3, -1158),
+    ["Tropics"]             = CFrame.new(4527, 3, 131),
+    ["Cherry Meadow"]       = CFrame.new(245, 59, 1147),
+    ["Volcano"]             = CFrame.new(-1585, 622, 1140),
+    ["Sand Island"]         = CFrame.new(2549, -5, -42),
+    ["Swamp"]               = CFrame.new(-1209, 132, -801),
+    ["Cavecrawler Cavern"]  = CFrame.new(3581, -179, 430),
+    ["Tiaga"]               = CFrame.new(898, 59, 1208),
+    ["Tiaga Peak"]          = CFrame.new(1465, 412, 3244),
+    ["Taiga Peak Cave"]     = CFrame.new(1493, 355, 3258),
+    ["River Tunnel"]        = CFrame.new(-1121, 0, -944),
+    ["Lone Cave"]           = CFrame.new(),
+ 
+}
